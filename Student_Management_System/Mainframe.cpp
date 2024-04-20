82e84c10cad8d5b49c9a7afe5554475dcdb9222a
Ammar Khan <ammar.khaan05@gmail.com>
2024-04-20 15:04:36 +0500

    Mouse and Keyboard Events

--- Student_Management_System/Mainframe.cpp
+++ Student_Management_System/Mainframe.cpp
@@ -4,16 +4,16 @@
 
 Mainframe::Mainframe(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel1 = new wxPanel(this);
-	wxButton* button1 = new wxButton(panel1, wxID_ANY, "Button 1", wxPoint(300,275), wxSize(200,50));
-	wxButton* button2 = new wxButton(panel1, wxID_ANY, "Button 2", wxPoint(300, 350), wxSize(200, 50));
-	CreateStatusBar();
+	
+	wxStatusBar* statusbar = CreateStatusBar();
+	panel1->Bind(wxEVT_KEY_DOWN, &Mainframe::OnKeyEvent, this);
+	statusbar->SetDoubleBuffered(true);
 
-	this->Bind(wxEVT_CLOSE_WINDOW, &Mainframe::OnClose, this);
-	this->Bind(wxEVT_BUTTON, &Mainframe::OnAnyButtonClicked, this);
-	button1->Bind(wxEVT_BUTTON, &Mainframe::OnButton1Clicked, this);
-	button2->Bind(wxEVT_BUTTON, &Mainframe::OnButton2Clicked, this);
 
 	/*
+	wxButton* button1 = new wxButton(panel1, wxID_ANY, "Button 1", wxPoint(300,275), wxSize(200,50));
+	wxButton* button2 = new wxButton(panel1, wxID_ANY, "Button 2", wxPoint(300, 350), wxSize(200, 50));
+	panel1->Bind(wxEVT_MOTION, &Mainframe::OnMouseEvent, this);
 	wxCheckBox* check1 = new wxCheckBox(panel1, wxID_ANY, "CheckBox", wxPoint(550, 50));
 	wxStaticText* text1 = new wxStaticText(panel1, wxID_ANY, "Hello World - Not editable", wxPoint(120, 100));
 	wxGauge* gauge1 = new wxGauge(panel1, wxID_ANY, 100, wxPoint(500, 255), wxSize(200, -1));
@@ -32,9 +32,27 @@ Mainframe::Mainframe(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	wxTextCtrl* msg = new wxTextCtrl(panel1, wxID_ANY, "Editable", wxPoint(300, 375), wxSize(200, -1));
 	slider1->Bind(wxEVT_SLIDER, &Mainframe::OnSliderChanged, this);
 	msg->Bind(wxEVT_TEXT, &Mainframe::OnTextChanged, this);
+	this->Bind(wxEVT_CLOSE_WINDOW, &Mainframe::OnClose, this);
+	this->Bind(wxEVT_BUTTON, &Mainframe::OnAnyButtonClicked, this);
+	button1->Bind(wxEVT_BUTTON, &Mainframe::OnButton1Clicked, this);
+	button2->Bind(wxEVT_BUTTON, &Mainframe::OnButton2Clicked, this);
 	*/
 }
 
+void Mainframe::OnKeyEvent(wxKeyEvent& evt) {
+	wxChar keyChar = evt.GetUnicodeKey();
+	wxLogStatus("Key Event %c",keyChar);
+}
+
+
+
+/*
+void Mainframe::OnMouseEvent(wxMouseEvent& evt) {
+	wxPoint mousePos = wxGetMousePosition();
+	mousePos = this->ScreenToClient(mousePos);
+	wxString msg = wxString::Format("Mouse Detected: x=%d, y=%d", mousePos.x, mousePos.y);
+	wxLogStatus(msg);
+}
 void Mainframe::OnClose(wxCloseEvent& evt) {
 	wxLogMessage("Frame Close");
 	evt.Skip();
@@ -52,8 +70,6 @@ void Mainframe::OnButton1Clicked(wxCommandEvent& evt) {
 void Mainframe::OnButton2Clicked(wxCommandEvent& evt) {
 	wxLogStatus("Button 2 Clicked");
 }
-
-/*
 void Mainframe::OnSliderChanged(wxCommandEvent& evt) {
 	wxString str = wxString::Format("Slider value: %d", evt.GetInt());
 	wxLogStatus(str);
--- Student_Management_System/Mainframe.h
+++ Student_Management_System/Mainframe.h
@@ -2,13 +2,16 @@
 #include<wx/wx.h>
 class Mainframe : public wxFrame{
 	private:
+		void OnKeyEvent(wxKeyEvent& evt);
+		/*
+		void OnMouseEvent(wxMouseEvent& evt);
 		void OnAnyButtonClicked(wxCommandEvent& evt);
 		void OnButton1Clicked(wxCommandEvent& evt);
 		void OnButton2Clicked(wxCommandEvent& evt);
 		void OnClose(wxCloseEvent& evt);
-
-		//void OnSliderChanged(wxCommandEvent& evt);
-		//void OnTextChanged(wxCommandEvent& evt);
+		void OnSliderChanged(wxCommandEvent& evt);
+		void OnTextChanged(wxCommandEvent& evt);
+		*/
 	public:
 		Mainframe(const wxString& title);
 };
