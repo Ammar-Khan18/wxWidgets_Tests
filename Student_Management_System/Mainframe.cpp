cf8e94b2dbdc7337787b413fa8ca9300dd2a23b8
Ammar Khan <ammar.khaan05@gmail.com>
2024-04-20 14:23:32 +0500

    Event Propogation

--- Student_Management_System/Mainframe.cpp
+++ Student_Management_System/Mainframe.cpp
@@ -4,19 +4,16 @@
 
 Mainframe::Mainframe(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel1 = new wxPanel(this);
-	wxButton* button2 = new wxButton(panel1, wxID_ANY, "Button", wxPoint(300, 275), wxSize(200, 50));
-	wxSlider* slider1 = new wxSlider(panel1, wxID_ANY, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
-	wxTextCtrl* msg = new wxTextCtrl(panel1, wxID_ANY, "Editable", wxPoint(300, 375), wxSize(200, -1));
+	wxButton* button1 = new wxButton(panel1, wxID_ANY, "Button 1", wxPoint(300,275), wxSize(200,50));
+	wxButton* button2 = new wxButton(panel1, wxID_ANY, "Button 2", wxPoint(300, 350), wxSize(200, 50));
 	CreateStatusBar();
 
-	button2->Bind(wxEVT_BUTTON, &Mainframe::OnButtonClicked, this);
-	slider1->Bind(wxEVT_SLIDER, &Mainframe::OnSliderChanged, this);
-	msg->Bind(wxEVT_TEXT, &Mainframe::OnTextChanged, this);
-
-	button2->Unbind(wxEVT_BUTTON, &Mainframe::OnButtonClicked, this);
+	this->Bind(wxEVT_CLOSE_WINDOW, &Mainframe::OnClose, this);
+	this->Bind(wxEVT_BUTTON, &Mainframe::OnAnyButtonClicked, this);
+	button1->Bind(wxEVT_BUTTON, &Mainframe::OnButton1Clicked, this);
+	button2->Bind(wxEVT_BUTTON, &Mainframe::OnButton2Clicked, this);
 
 	/*
-	wxButton* button1 = new wxButton(panel1, wxID_ANY, "Button", wxPoint(150,50), wxSize(100,35));
 	wxCheckBox* check1 = new wxCheckBox(panel1, wxID_ANY, "CheckBox", wxPoint(550, 50));
 	wxStaticText* text1 = new wxStaticText(panel1, wxID_ANY, "Hello World - Not editable", wxPoint(120, 100));
 	wxGauge* gauge1 = new wxGauge(panel1, wxID_ANY, 100, wxPoint(500, 255), wxSize(200, -1));
@@ -30,13 +27,33 @@ Mainframe::Mainframe(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	wxSpinCtrl* spin = new wxSpinCtrl(panel1, wxID_ANY, "", wxPoint(550, 375), wxSize(100, -1));
 	wxListBox* listBox = new wxListBox(panel1, wxID_ANY, wxPoint(150, 475), wxSize(100, -1), choices); // Add and delete strings at runtime
 	wxRadioBox* radBox = new wxRadioBox(panel1, wxID_ANY, "Radio", wxPoint(485, 475), wxDefaultSize, choices);
+	// Dynamic event handling
+	wxSlider* slider1 = new wxSlider(panel1, wxID_ANY, 0, 0, 100, wxPoint(300, 200), wxSize(200, -1));
+	wxTextCtrl* msg = new wxTextCtrl(panel1, wxID_ANY, "Editable", wxPoint(300, 375), wxSize(200, -1));
+	slider1->Bind(wxEVT_SLIDER, &Mainframe::OnSliderChanged, this);
+	msg->Bind(wxEVT_TEXT, &Mainframe::OnTextChanged, this);
 	*/
 }
 
-void Mainframe::OnButtonClicked(wxCommandEvent& evt) {
-	wxLogStatus("Button Clicked");
+void Mainframe::OnClose(wxCloseEvent& evt) {
+	wxLogMessage("Frame Close");
+	evt.Skip();
 }
 
+void Mainframe::OnAnyButtonClicked(wxCommandEvent& evt) {
+	wxLogMessage("Button Clicked");
+}
+
+void Mainframe::OnButton1Clicked(wxCommandEvent& evt) {
+	wxLogStatus("Button 1 Clicked");
+	evt.Skip();
+}
+
+void Mainframe::OnButton2Clicked(wxCommandEvent& evt) {
+	wxLogStatus("Button 2 Clicked");
+}
+
+/*
 void Mainframe::OnSliderChanged(wxCommandEvent& evt) {
 	wxString str = wxString::Format("Slider value: %d", evt.GetInt());
 	wxLogStatus(str);
@@ -46,3 +63,4 @@ void Mainframe::OnTextChanged(wxCommandEvent& evt) {
 	wxString str = wxString::Format("Text: %s", evt.GetString());
 	wxLogStatus(str);
 }
+*/
--- Student_Management_System/Mainframe.h
+++ Student_Management_System/Mainframe.h
@@ -2,9 +2,13 @@
 #include<wx/wx.h>
 class Mainframe : public wxFrame{
 	private:
-		void OnButtonClicked(wxCommandEvent& evt);
-		void OnSliderChanged(wxCommandEvent& evt);
-		void OnTextChanged(wxCommandEvent& evt);
+		void OnAnyButtonClicked(wxCommandEvent& evt);
+		void OnButton1Clicked(wxCommandEvent& evt);
+		void OnButton2Clicked(wxCommandEvent& evt);
+		void OnClose(wxCloseEvent& evt);
+
+		//void OnSliderChanged(wxCommandEvent& evt);
+		//void OnTextChanged(wxCommandEvent& evt);
 	public:
 		Mainframe(const wxString& title);
 };
